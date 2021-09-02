08443834cb0d45a91b926d587c3ad217ea84cee8
Rostislav Kulik <45662763+r-kulik@users.noreply.github.com>
2021-09-02 21:02:55 +0300

    start

--- main.py
+++ main.py
@@ -1,166 +1,64 @@
-# -*- coding: utf-8 -*-
+from os import curdir
+from sqlite3.dbapi2 import Cursor
+import telebot
+import config
+import sources
+import sqlite3
 
-import json
-import os
-import random
-import sys
-import time
+bot = telebot.TeleBot(config.token)
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+print("started working")
 
-from helper import parser, boolean, rememberAll, toDate
-from main_ui import Ui_MainWindow
+def add_user(user_id : int) -> None:
+    database_connection = sqlite3.connect('data.db'); cursor = database_connection.cursor()
+    status = 0; tickets = 0
+    cursor.execute("INSERT INTO users VALUES ({}, {}, {});".format(user_id, status, tickets))
+    database_connection.commit()
+    database_connection.close()
 
 
-class Example(QMainWindow, Ui_MainWindow):
+def set_status(user_id : int, status : int) -> None:
+    database_connection = sqlite3.connect("data.db"); cursor = database_connection.cursor()
+    cursor.execute("SELECT EXISTS(SELECT 1 FROM users WHERE id='{}')".format(user_id))
+    fetchresult : tuple = cursor.fetchone()
+    print(fetchresult)
+    if fetchresult == (0,):
+        database_connection.close()
+        add_user(user_id)
+    database_connection = sqlite3.connect("data.db"); cursor = database_connection.cursor()
+    cursor.execute("UPDATE users SET status={} WHERE id={}".format(status, user_id))
+    database_connection.commit()
 
-    def __init__(self):
-        self.current_version = '1.0'
-        super().__init__()
-        self.setupUi(self)
-        self.window_width = 800
-        self.window_height = 529
-        self.setWindowTitle("Гермес - Финансовый помощник")
-        palette = QPalette()
-        self.setPalette(palette)
-        self.label_4.setText(self.current_version)
-        self.pushButton.clicked.connect(self.add_transaction)
-        transaction_types = ['Заработок',
-                             'Трата',
-                             'Взятие в долг',
-                             'Предоставление долга']
-        self.comboBox_2.addItems(transaction_types)
-        self.comboBox_2.activated[str].connect(self.combox2Active)
-        self.type_dict = {"Заработок": "reciept",
-                          "Трата": "spend",
-                          "Взятие в долг": "borrow",
-                          "Предоставление долга": "loan"}
-        init_tuple = rememberAll()
-        self.balance = init_tuple[0]
-        self.debt = init_tuple[1]
-        self.listWidget.addItems(init_tuple[2])
-        parser(self)
-        self.pushButton.setText("Добавить")
-        self.pushButton_5.clicked.connect(self.clearCash)
-        self.pushButton_4.clicked.connect(self.showSettings)
-        self.pushButton_3.clicked.connect(self.showStatistics)
 
-    def showStatistics(self):
-    	os.popen("statistics.py")
-    	
-    def showSettings(self):
-        os.popen("typesettingswindow.py")
+def check_status(user_id : int) -> int:
+    database_connection = sqlite3.connect('data.db'); cursor = database_connection.cursor()
 
-    def add_transaction(self):
-        with open("inoutcome.json") as inoutcome:
-            a = inoutcome.read()
-            inoutcome_dictionary = json.loads(a)
-        # print(inoutcome_dictionary)
-        current_time = int(time.time())
-        written_dict = {}
-        # print(inoutcome_dictionary)
-        try:
-            written_dict["name"] = self.lineEdit.text()
-            written_dict["type"] = self.type_dict[self.combotext2]
-            written_dict["operand"] = self.combotext
-            written_dict["summ"] = float(self.lineEdit_2.text())
-        except Exception:
-            self.label_4.setText("Введите все или введите корректно")
-        else:
-            self.label_4.setText(self.current_version)
-            if str(current_time) in inoutcome_dictionary:
-                current_time += 1
-            inoutcome_dictionary[str(current_time)] = written_dict
-            # print(inoutcome_dictionary)
-            # print(written_dict)
-            # print(written_dict["type"])["summ"]
-            if written_dict["type"] == "spend":
-                self.balance -= written_dict["summ"]
-            elif written_dict["type"] == "reciept":
-                self.balance += written_dict["summ"]
-            elif written_dict["type"] == "loan":
-                self.balance -= written_dict["summ"]
-                self.debt += written_dict["summ"]
-            elif written_dict["type"] == "borrow":
-                # print('DONE')
-                # print(self.balance, self.debt)
-                # print(written_dict["summ"])
-                self.balance = self.balance + written_dict["summ"]
-                self.debt = self.debt - written_dict["summ"]
-            # print(self.balance, self.debt)
 
-            # print(inoutcome_dictionary)
-            with open("inoutcome.json", 'w') as inoutcome:
-                json.dump(inoutcome_dictionary, inoutcome)
-        try:
-            # print(self.balance, self.debt)
-            parser(self)
-            short_report = ' '.join([toDate(str(current_time)),
-                                     written_dict["name"],
-                                     boolean(written_dict["type"]),
-                                     str(written_dict["summ"])])
-            self.listWidget.clear()
-            self.listWidget.addItems(rememberAll()[2])
-            self.label_4.setText(self.current_version)
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = telebot.types.ReplyKeyboardMarkup()
+    order_button = telebot.types.KeyboardButton(sources.order_button)
+    accept_button = telebot.types.KeyboardButton(sources.picking_button)
+    my_orders_button = telebot.types.KeyboardButton(sources.my_orders_button)
+    markup.row(order_button)
+    markup.row(accept_button, my_orders_button)
+    bot.send_message(message.chat.id, sources.greeting, reply_markup=markup)
 
-        except Exception:
+@bot.message_handler()
+def handle_message(message):
+    text : str = message.text
+    user_id : int = message.chat.id
+    if text == sources.order_button:
+        bot.send_message(message.chat.id, sources.order_question, reply_markup=None)
+        set_status(user_id, sources.input_order_status)
+    elif text == sources.picking_button:
+        pass
+    elif text == sources.my_orders_button:
+        pass
+    else:
+        user_status = check_status(user_id)
+        if user_status == sources.input_order_status:
             pass
 
-    def combox2Active(self, text):
-        self.combotext2 = text
-        with open('transaction_types.json', 'r', encoding='utf-8') as transaction_types_file:
-            transaction_types = json.loads(transaction_types_file.read())
-        if text == 'Заработок':
-            transaction_types_income = transaction_types["reciept"]
-            self.comboBox.clear()
-            self.comboBox.addItems(transaction_types_income)
-            self.comboBox.activated[str].connect(self.comboxActive)
-        elif text == 'Трата':
-            transaction_types_outcome = transaction_types['spend']
-            self.comboBox.clear()
-            self.comboBox.addItems(transaction_types_outcome)
-            self.comboBox.activated[str].connect(self.comboxActive)
 
-        elif text == 'Взятие в долг':
-            self.comboBox.clear()
-            self.comboBox.addItems(transaction_types["borrow"])
-            self.comboBox.activated[str].connect(self.comboxActive)
-
-        elif text == 'Предоставление долга':
-            self.comboBox.clear()
-            self.comboBox.addItems(transaction_types["loan"])
-            self.comboBox.activated[str].connect(self.comboxActive)
-
-    def comboxActive(self, text2):
-        self.combotext = text2
-
-    # print(self.combotext2, self.combotext)
-
-    def clearCash(self):
-        s1 = random.randint(1, 11)
-        s2 = random.randint(1, 11)
-        summ = s1 + s2
-        i, okBtnPressed = QInputDialog.getInt(self,
-                                              "Вы действительно хотите удалить все?",
-                                              "Сколько будет " + str(s1) + '+' + str(s2),
-                                              0, 0, 100, 1)
-        if okBtnPressed:
-            if i == summ:
-                with open("inoutcome.json", 'w') as inoutcome:
-                    inoutcome.write('{}')
-                init_tuple = rememberAll()
-                self.balance = init_tuple[0]
-                self.debt = init_tuple[1]
-                self.listWidget.clear()
-                self.listWidget.addItems(init_tuple[2])
-                parser(self)
-            else:
-                self.label_4.setText('Неправильно решен пример лол')
-
-
-app = QApplication(sys.argv)
-ex = Example()
-ex.show()
-sys.exit(app.exec_())
+bot.polling()
